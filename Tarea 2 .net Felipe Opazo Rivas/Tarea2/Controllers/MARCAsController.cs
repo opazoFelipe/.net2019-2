d66e23e508bb12025f247f6816b2fcfa0360f199
felipe <felipe.orivas@gmail.com>
2020-03-11 22:38:48 -0300

    tarea 2 terminada

---
+++ Tarea 2 .net Felipe Opazo Rivas/Tarea2/Controllers/MARCAsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tarea2.Models;
+
+namespace Tarea2.Controllers
+{
+    public class MARCAsController : Controller
+    {
+        private testEntities db = new testEntities();
+
+        // GET: MARCAs
+        public ActionResult Index()
+        {
+            return View(db.MARCA.ToList());
+        }
+
+        // GET: MARCAs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MARCA mARCA = db.MARCA.Find(id);
+            if (mARCA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mARCA);
+        }
+
+        // GET: MARCAs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MARCAs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_MARCA,DESCRIPCION")] MARCA mARCA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MARCA.Add(mARCA);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mARCA);
+        }
+
+        // GET: MARCAs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MARCA mARCA = db.MARCA.Find(id);
+            if (mARCA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mARCA);
+        }
+
+        // POST: MARCAs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_MARCA,DESCRIPCION")] MARCA mARCA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mARCA).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mARCA);
+        }
+
+        // GET: MARCAs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MARCA mARCA = db.MARCA.Find(id);
+            if (mARCA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mARCA);
+        }
+
+        // POST: MARCAs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MARCA mARCA = db.MARCA.Find(id);
+            db.MARCA.Remove(mARCA);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
